1f84af81bd15a460fbd14cef21008fa094c153fe
natmp2 <126459403+natmp2@users.noreply.github.com>
2024-04-22 02:57:12 -0600

    Update HorarioAtencion.java

--- HorarioAtencion.java
+++ HorarioAtencion.java
@@ -14,8 +14,32 @@ public HorarioAtencion(LocalDate fecha, int horaApertura, int horaCierre){
     this.horaApertura = horaApertura;
     this.horaCierre = horaCierre; 
 }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getHoraApertura() {
+        return horaApertura;
+    }
+
+    public void setHoraApertura(int horaApertura) {
+        this.horaApertura = horaApertura;
+    }
+
+    public int getHoraCierre() {
+        return horaCierre;
+    }
+
+    public void setHoraCierre(int horaCierre) {
+        this.horaCierre = horaCierre;
+    }
     
-    
+
     
     
     
