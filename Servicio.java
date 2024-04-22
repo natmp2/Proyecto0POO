82069c30355f204ace029b6577f7a4a50a498c29
natmp2 <126459403+natmp2@users.noreply.github.com>
2024-04-22 02:56:55 -0600

    Update Servicio.java

--- Servicio.java
+++ Servicio.java
@@ -15,7 +15,39 @@ public Servicio(String nombre){
     this.numero = consecutivo++;
 }
 
-    
+    public static int getConsecutivo() {
+        return consecutivo;
+    }
+
+    public static void setConsecutivo(int consecutivo) {
+        Servicio.consecutivo = consecutivo;
+    }
+
+    public static int getDuracion() {
+        return duracion;
+    }
+
+    public static void setDuracion(int duracion) {
+        Servicio.duracion = duracion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
     
     
 }
