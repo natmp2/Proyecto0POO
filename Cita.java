10235a9018ff4a1aa70229b6321f0d46ae8d087d
natmp2 <126459403+natmp2@users.noreply.github.com>
2024-04-22 02:57:28 -0600

    Update Cita.java

--- Cita.java
+++ Cita.java
@@ -20,6 +20,56 @@ public Cita (LocalDate fecha, int hora, Servicio servicio, boolean confirmacion,
     this.servicio = servicio;
 }
 
+    public static int getConsecutivo() {
+        return consecutivo;
+    }
+
+    public static void setConsecutivo(int consecutivo) {
+        Cita.consecutivo = consecutivo;
+    }
+
+    public boolean isConfirmacion() {
+        return confirmacion;
+    }
+
+    public void setConfirmacion(boolean confirmacion) {
+        this.confirmacion = confirmacion;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio) {
+        this.servicio = servicio;
+    }
+
+
+
 @Override
 public String toString(){
     return "";
