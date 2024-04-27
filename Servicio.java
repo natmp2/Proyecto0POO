381be03c7f883a77abfd54ddb977e178afd596c7
pipao1229 <126714967+pipao1229@users.noreply.github.com>
2024-04-27 12:09:28 -0600

    Update Servicio.java

--- Servicio.java
+++ Servicio.java
@@ -1,67 +1,35 @@
 package itcr.proyecto0poo;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Servicio implements Serializable {
     private static int consecutivo = 1;
-    private static int duracion = 60;
     private int numero;
     private String nombre;
-    public static List<Servicio> listaServicios = new ArrayList<>();
 
     public Servicio(String nombre) {
         this.nombre = nombre;
         this.numero = consecutivo++;
     }
 
-    public static void crearServicio(String nombre) {
+    public void crearServicio(String nombre) {
         Servicio nuevoServicio = new Servicio(nombre);
-        listaServicios.add(nuevoServicio);
         System.out.println("Servicio creado exitosamente: " + nuevoServicio);
     }
     
-    public static void modificarServicio(int numeroServicio, String nuevoNombre) {
-    // verificar si el servicio existe
-    if (numeroServicio >= 1 && numeroServicio <= listaServicios.size()) {
-        Servicio servicio = listaServicios.get(numeroServicio - 1); // restar 1 para ajustar a 0-based
-        servicio.setNombre(nuevoNombre);
-        System.out.println("Servicio modificado exitosamente: " + servicio);
-    } else {
-        System.out.println("El servicio seleccionado no existe.");
-        }
+    public void modificarServicio(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+        System.out.println("Servicio modificado exitosamente: " + this);
     }
     
-    public static void borrarServicio(int numeroServicio, List<Cita> citas) {
-        if (numeroServicio >= 1 && numeroServicio <= listaServicios.size()) {
-            Servicio servicio = listaServicios.get(numeroServicio - 1);
-
-            // verificar si el servicio está siendo utilizado en alguna cita
-            for (Cita cita : citas) {
-                if (cita.getServicio().equals(servicio)) {
-                    System.out.println("No se puede eliminar el tipo de servicio porque está siendo utilizado en una cita programada.");
-                    return;
-                }
-            }
-
-            // si el servicio no está siendo utilizado, se elimina
-            listaServicios.remove(servicio);
-            System.out.println("Servicio eliminado exitosamente: " + servicio);
-        } else {
-            System.out.println("El servicio seleccionado no existe.");
-        }
+    public void borrarServicio() {
+        System.out.println("Servicio eliminado exitosamente: " + this);
     }
 
-    public static void consultarServicio(int numeroServicio) {
-        if (numeroServicio >= 1 && numeroServicio <= listaServicios.size()) {
-            Servicio servicio = listaServicios.get(numeroServicio - 1);
-            System.out.println("Datos del tipo de servicio:");
-            System.out.println("Número: " + servicio.getNumero());
-            System.out.println("Nombre: " + servicio.getNombre());
-        } else {
-            System.out.println("El servicio seleccionado no existe.");
-        }
+    public void consultarServicio() {
+        System.out.println("Datos del tipo de servicio:");
+        System.out.println("Número: " + this.numero);
+        System.out.println("Nombre: " + this.nombre);
     }
 
     public static int getConsecutivo() {
@@ -72,13 +40,6 @@ public class Servicio implements Serializable {
         Servicio.consecutivo = consecutivo;
     }
 
-    public static int getDuracion() {
-        return duracion;
-    }
-
-    public static void setDuracion(int duracion) {
-        Servicio.duracion = duracion;
-    }
 
     public int getNumero() {
         return numero;
@@ -100,5 +61,4 @@ public class Servicio implements Serializable {
     public String toString() {
         return "Número: " + this.numero + ", Nombre: " + this.nombre;
     }
-
 }
